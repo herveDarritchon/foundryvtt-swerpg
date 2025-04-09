e74c00bde13002fa1d4682ba9337a92764c88a88
Hervé Darritchon <herve.darritchon@gmail.com>
2025-04-09 21:50:18 +0200

    [WIP] Make error-skill.test.mjs coherent with production code (coverage 100%).

--- tests/lib/skills/error-skill.test.mjs
+++ tests/lib/skills/error-skill.test.mjs
@@ -6,20 +6,7 @@ import {createSkill} from "../../utils/skills/skill.mjs";
 import ErrorSkill from "../../../module/lib/skills/error-skill.mjs";
 
 describe('Error Skill', () => {
-    describe('train a skill', () => {
-        test('should create an error skill with correct message', () => {
-            const actor = createActor();
-            const skill = createSkill();
-            const params = {};
-            const options = {};
-
-            const errorSkill = new ErrorSkill(actor, skill, params, options);
-            const trainErrorSkill = errorSkill.train();
-
-            expect(trainErrorSkill.options.message).toBe("Train not implemented. Should not be used!");
-        });
-    });
-    describe('forget a skill', () => {
+    describe('process a skill', () => {
         test('should create an error skill with correct message', () => {
             const actor = createActor({careerSpent: 1});
             const skill = createSkill({careerFree: 1});
@@ -27,23 +14,47 @@ describe('Error Skill', () => {
             const options = {};
 
             const errorSkill = new ErrorSkill(actor, skill, params, options);
-            const forgetErrorSkill = errorSkill.forget();
+            const forgetErrorSkill = errorSkill.process();
 
-            expect(forgetErrorSkill.options.message).toBe("Forget not implemented. Should not be used!");
+            expect(forgetErrorSkill.options.message).toBe("Process not implemented. Should not be used!");
         });
-    });
-    describe('evaluate a skill', () => {
-        test('should create an error skill with correct message', () => {
-            const actor = createActor({careerSpent: 1});
-            const skill = createSkill({careerFree: 1});
-            const params = {};
-            const options = {};
+        describe('train a skill', () => {
+            test('should create an error skill with correct message', () => {
+                const actor = createActor();
+                const skill = createSkill();
+                const params = {
+                    action: "train",
+                    isCreation: true,
+                    isCareer: true,
+                    isSpecialization: true
+                };
+                const options = {};
 
-            const errorSkill = new ErrorSkill(actor, skill, params, options);
-            const forgetErrorSkill = errorSkill.evaluate();
+                const errorSkill = new ErrorSkill(actor, skill, params, options);
+                const trainErrorSkill = errorSkill.process();
 
-            expect(forgetErrorSkill.options.message).toBe("Evaluate not implemented. Should not be used!");
+                expect(trainErrorSkill.options.message).toBe("Process not implemented. Should not be used!");
+            });
         });
+        describe('forget a skill', () => {
+            test('should create an error skill with correct message', () => {
+                const actor = createActor({careerSpent: 1});
+                const skill = createSkill({careerFree: 1});
+                const params = {
+                    action: "forget",
+                    isCreation: true,
+                    isCareer: true,
+                    isSpecialization: true
+                };
+                const options = {};
+
+                const errorSkill = new ErrorSkill(actor, skill, params, options);
+                const forgetErrorSkill = errorSkill.process();
+
+                expect(forgetErrorSkill.options.message).toBe("Process not implemented. Should not be used!");
+            });
+        });
+
     });
     describe('updateState a skill', () => {
         test('should create an error skill with correct message', async () => {
