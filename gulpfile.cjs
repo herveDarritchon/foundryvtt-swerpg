1e8f6709f25493201454ca943258b908d64681df
Hervé Darritchon <herve.darritchon@gmail.com>
2025-04-20 19:36:11 +0200

    feat(characteristics): wip - setup characteristics management and icons.

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -1,20 +1,31 @@
-let gulp = require('gulp');
-let less = require('gulp-less');
+const gulp = require('gulp');
+const less = require('gulp-less');
+const touch = require('gulp-touch-cmd');
+const plumber = require('gulp-plumber');
 
 gulp.task('less', function () {
     return gulp
         .src('./styles/swerpg.less')
         .pipe(less())
-        .pipe(gulp.dest('./styles'));
+        .pipe(gulp.dest('./styles'))
+        .pipe(touch()); // force update pour que Foundry détecte le changement
 });
 
 gulp.task('styles', function () {
     return gulp
         .src('./styles/swerpg.less')
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.error('LESS Error:', err.message);
+                this.emit('end');
+            }
+        }))
         .pipe(less())
-        .pipe(gulp.dest('./styles/'));
+        .pipe(gulp.dest('./styles/'))
+        .pipe(touch())
+        .on('end', () => console.log('✅ swerpg.css compiled and updated'));
 });
 
-gulp.task('default', function () {
+gulp.task('default', gulp.series('styles', function () {
     return gulp.watch('./styles/**/*.less', gulp.series('styles'));
-});
+}));
