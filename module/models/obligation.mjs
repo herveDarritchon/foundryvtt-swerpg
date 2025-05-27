ae7f7608004fc6ccf591be1826f1df4eef177387
Hervé Darritchon <herve.darritchon@gmail.com>
2025-05-27 10:40:36 +0200

    feat(obligation): add Obligation Sheet and related data schema for managing obligations

---
+++ module/models/obligation.mjs
@@ -0,0 +1,66 @@
+/**
+ * Data schema, attributes, and methods specific to Ancestry type Items.
+ */
+export default class SwerpgObligation extends foundry.abstract.TypeDataModel {
+
+    /* -------------------------------------------- */
+    /*  Data Schema                                 */
+
+    /* -------------------------------------------- */
+
+    /** @inheritDoc */
+    static defineSchema() {
+        const fields = foundry.data.fields;
+        const schema = {};
+
+        schema.description = new fields.HTMLField({required: false, initial: undefined});
+
+        schema.value = new fields.NumberField({
+            required: true,
+            integer: true,
+            nullable: false,
+            min: 0,
+            max: 50,
+            initial: 10,
+            step: 5
+        });
+
+        schema.isExtra = new fields.BooleanField({
+            required: false,
+            nullable: false,
+            initial: false
+        });
+
+        schema.extraXp = new fields.NumberField({
+            required: true,
+            integer: true,
+            nullable: false,
+            min: 0,
+            max: 20,
+            initial: 0,
+            step: 5
+        });
+
+        schema.extraMoney = new fields.NumberField({
+            required: true,
+            integer: true,
+            nullable: false,
+            min: 0,
+            max: 5000,
+            initial: 0,
+            step: 500
+        });
+
+        return schema;
+    }
+
+    /** @override */
+    static LOCALIZATION_PREFIXES = ["OBLIGATION"];
+
+    /* -------------------------------------------- */
+
+    /** @inheritdoc */
+    static validateJoint(data) {
+    }
+
+}
