9a1ca6806de46d5517f8097c22a85cd8cd706ff9
Hervé Darritchon <herve.darritchon@gmail.com>
2025-06-04 08:49:41 +0200

    chore: add .gitkeep files for directory structure

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,8 @@
 export default {
-  input: "swerpg.mjs",
-  output: {
-    file: "crucible-swerpg.mjs",
-    format: "es",
-    compact: true
-  }
+    input: "swerpg.mjs",
+    output: {
+        file: "crucible-swerpg.mjs",
+        format: "es",
+        compact: true
+    }
 }
