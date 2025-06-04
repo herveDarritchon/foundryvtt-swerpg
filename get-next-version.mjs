3afe6b9debd66b05d893bb7336039912c47b03bd
Hervé Darritchon <herve.darritchon@gmail.com>
2025-06-04 12:03:29 +0200

    chore: update get-next-version script and release.yml for semantic-release plugins

---
+++ get-next-version.mjs
@@ -0,0 +1,21 @@
+import semanticRelease from 'semantic-release';
+
+const result = await semanticRelease({
+    dryRun: true,
+    branches: ['main'],
+    repositoryUrl: 'https://github.com/herveDarritchon/foundryvtt-swerpg',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+    ]
+}, {
+    cwd: process.cwd(),
+    env: process.env,
+});
+
+if (result?.nextRelease?.version) {
+    console.log(result.nextRelease.version);
+} else {
+    console.log('NO_RELEASE');
+}
