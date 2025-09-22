12d1acb68677e03da1b838358f048aacf8cf5031
Hervé Darritchon <herve.darritchon@gmail.com>
2025-09-22 22:08:24 +0200

    chore(module): remove SwerpgScene from exports

--- module/documents/_module.mjs
+++ module/documents/_module.mjs
@@ -3,7 +3,6 @@ export {default as SwerpgChatMessage} from "./chat-message.mjs";
 export {default as SwerpgCombat} from "./combat.mjs";
 export {default as SwerpgCombatant} from "./combatant.mjs";
 export {default as SwerpgItem} from "./item.mjs";
-export {default as SwerpgScene} from "./scene.mjs";
 export {default as SwerpgToken} from "./token.mjs";
 
 
