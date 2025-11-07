03bcc1f06d952429bdb5f7ee7b1e224e8c6ae33c
Hervé Darritchon <herve.darritchon@gmail.com>
2025-11-07 09:21:21 +0100

    feat: Ajout de la configuration ESLint et Prettier, remplacement de Gulp par un nouveau fichier de configuration pour la compilation LESS

---
+++ gulpfile.mjs
@@ -0,0 +1,43 @@
+import gulp from "gulp"
+import less from "gulp-less"
+
+/* ----------------------------------------- */
+/*  Compile LESS
+/* ----------------------------------------- */
+const LESS_SRC = "./styles/swerpg.less"
+const CSS_DEST = "./styles"
+const LESS_WATCH = ["./styles/**/*.less"]
+
+/**
+ *
+ */
+function compileLESS() {
+  console.log("Compiling LESS files from:", LESS_SRC, "to:", CSS_DEST)
+  return gulp
+    .src(LESS_SRC)
+    .pipe(
+      less().on("error", function (err) {
+        console.error("LESS Error:", err.message)
+        this.emit("end")
+      }),
+    )
+    .pipe(gulp.dest(CSS_DEST))
+}
+const css = gulp.series(compileLESS)
+
+/* ----------------------------------------- */
+/*  Watch Updates
+/* ----------------------------------------- */
+/**
+ *
+ */
+function watchUpdates() {
+  gulp.watch(LESS_WATCH, css)
+}
+
+/* ----------------------------------------- */
+/*  Export Tasks
+/* ----------------------------------------- */
+
+export { css }
+export default gulp.series(gulp.parallel(css), watchUpdates)
