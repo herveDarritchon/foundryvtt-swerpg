3d6c672d25a542ff4c8a61a7fe41af2583b3f982
Hervé Darritchon <herve.darritchon@gmail.com>
2025-11-11 22:48:47 +0100

    feat(oggdude-import): add OggDude data importer functionality
    Implement the OggDude data importer with features for importing gear, armor, weapons, species, and careers. This includes new modules for parsing XML data, creating folders in the Foundry VTT sidebar, and building context for the imported data. Additionally, update settings for the OggDude Data Importer and enhance the user interface for file selection and domain selection.

---
+++ module/importer/items/gear-ogg-dude.mjs
@@ -0,0 +1,97 @@
+import {buildArmorImgWorldPath, buildItemImgSystemPath} from "../../settings/directories.mjs";
+import OggDudeImporter from "../oggDude.mjs";
+import {buildMod, buildWeaponModifiers} from "./combat-item-mapper.mjs";
+import OggDudeDataElement from "../../settings/models/OggDudeDataElement.mjs";
+
+/**
+ * Gear Array Mapper : Map the Gear XML data to the SwerpgGear object array.
+ * @param gears {Array} The Gears data from the XML file.
+ * @returns {Array} The Gear object array.
+ * @public
+ * @function
+ * @name gearMapper
+ */
+export function gearMapper(gears) {
+    return gears.map((xmlGear) => {
+        return {
+            short: OggDudeImporter.mapOptionalString(xmlGear.Short),
+
+            // FIXME il faudrait refactorer tout ça pour ne pas avoir de duplication avec les autres combat-item
+            name: OggDudeImporter.mapMandatoryString("gear.Name", xmlGear.Name),
+            key: OggDudeImporter.mapMandatoryString("gear.Key", xmlGear.Key),
+            description: OggDudeImporter.mapMandatoryString("gear.Description", xmlGear.Description),
+            restricted: OggDudeImporter.mapOptionalBoolean(xmlGear.Restricted),
+            encumbrance: OggDudeImporter.mapOptionalNumber(xmlGear.Encumbrance),
+            price: OggDudeImporter.mapOptionalNumber(xmlGear.Price),
+            rarity: OggDudeImporter.mapOptionalNumber(xmlGear.Rarity),
+            hp: OggDudeImporter.mapOptionalNumber(xmlGear.HP),
+            type: OggDudeImporter.mapMandatoryString("gear.Type", xmlGear.Type),
+
+            sources: OggDudeImporter.mapOptionalArray(
+                xmlGear?.Sources?.Source,
+                (source) => {
+                    return {description: source._, page: source.Page}
+                }),
+
+            categories: OggDudeImporter.mapOptionalArray(xmlGear?.Categories?.Category, (category) => category),
+
+            mods: OggDudeImporter.mapOptionalArray(
+                xmlGear?.BaseMods?.Mod,
+                (mod) => {
+                    return buildMod(mod)
+                }
+            ),
+
+            weaponModifiers: OggDudeImporter.mapOptionalArray(xmlGear?.WeaponModifiers?.WeaponModifier, (weaponModifier) => buildWeaponModifiers(weaponModifier)),
+
+            eraPricing: OggDudeImporter.mapOptionalArray(xmlGear?.EraPricing?.Era, (eraPrice) => {
+                return {
+                    name: OggDudeImporter.mapMandatoryString("gear.EraPrice.Name", eraPrice.Name),
+                    price: OggDudeImporter.mapMandatoryString("gear.EraPrice.Price", eraPrice.Price),
+                    rarity: OggDudeImporter.mapMandatoryString("gear.EraPrice.Rarity", eraPrice.Rarity),
+                    restricted: OggDudeImporter.mapMandatoryBoolean("gear.EraPrice.Restricted", eraPrice.Restricted)
+                }
+            })
+
+        }
+    });
+}
+
+/**
+ * Build the Gear context for the importer process.
+ * @param zip
+ * @param groupByDirectory
+ * @param groupByType
+ * @returns {{zip: {elementFileName: string, directories, content}, image: {images: (string|((buffer: Buffer, options?: ansiEscapes.ImageOptions) => string)|number|[OggDudeDataElement]|[OggDudeDataElement,OggDudeDataElement]|[OggDudeDataElement,OggDudeDataElement]|OggDudeContextImage|*), criteria: string, systemPath: string, worldPath: string}, folder: {name: string, type: string}, element: {jsonCriteria: string, mapper: *, type: string}}}
+ * @public
+ * @function
+ */
+export async function buildGearContext(zip, groupByDirectory, groupByType) {
+
+    console.debug("Building Gear with Zip, GroupByDirectory, GroupByType", zip, groupByDirectory, groupByType);
+
+    return {
+        jsonData: await OggDudeDataElement.buildJsonDataFromFile(zip, groupByDirectory, "Gear.xml", "Gears.Gear"),
+        zip: {
+            elementFileName: "Gear.xml",
+            content: zip,
+            directories: groupByDirectory
+        },
+        image: {
+            criteria: "Data/EquipmentImages/Gear",
+            worldPath: buildArmorImgWorldPath("gears"),
+            systemPath: buildItemImgSystemPath("gear.svg"),
+            images: groupByType.image,
+            prefix: 'Gear'
+        },
+        folder: {
+            name: 'Swerpg - Gears',
+            type: 'Item'
+        },
+        element: {
+            jsonCriteria: 'Gears.Gear',
+            mapper: gearMapper,
+            type: 'gear'
+        }
+    };
+}
---
+++ module/settings/OggDudeDataImporter.mjs
@@ -0,0 +1,203 @@
+/**
+ * @typedef {object} FormApplication
+ * @typedef {object} NewDataFile
+ * @property {string} [src=""]          The OggDude Data file.
+ */
+
+import OggDudeImporter from "../importer/oggDude.mjs";
+// Similar syntax to importing, but note that
+// this is object destructuring rather than an actual import
+
+const {ApplicationV2, HandlebarsApplicationMixin} = foundry.applications.api
+
+
+/**
+ * An application for processing OggDude data file. This application is used to import data from OggDude's generator.
+ * @extends {FormApplication}
+ */
+export class OggDudeDataImporter extends HandlebarsApplicationMixin(ApplicationV2) {
+
+    _domainNames = ["weapon", "armor", "gear", "species", "career"];
+
+    domains = this._initializeDomains(this._domainNames);
+    zipFile = null;
+
+    /* -------------------------------------------- */
+
+    /**
+     * Initialize the domains for the OggDude data importer.
+     * @param domainNames {string[]} The names of the domains to initialize.
+     * @returns {object[]} The initialized domains.
+     * @private
+     */
+    _initializeDomains(domainNames) {
+        return domainNames.map(name => {
+            return {
+                id: name,
+                label: `SETTINGS.OggDudeDataImporter.loadWindow.domains.${name}`,
+                checked: false
+            };
+        });
+    }
+
+    /* -------------------------------------------- */
+
+    static PARTS = {
+        swerpgSettings: {
+            template: 'systems/swerpg/templates/settings/oggDudeDataImporter.hbs',
+        }
+    }
+
+    /* -------------------------------------------- */
+
+    /** @inheritdoc */
+    static DEFAULT_OPTIONS = {
+        id: 'swerpgSettings-form',
+        tag: "form",
+        form: {
+            handler: OggDudeDataImporter._onSubmit,
+            closeOnSubmit: false,
+            submitOnChange: false,
+        },
+        window: {
+            icon: 'fas fa-cogs',
+            title: 'SETTINGS.OggDudeDataImporter.loadWindow.menuLabel',
+            contentClasses: ["standard-form"],
+        },
+        //popOut: true,
+        position: {
+            width: 640,
+            height: "auto",
+        },
+        actions: {
+            resetAction: OggDudeDataImporter.resetAction,
+            loadAction: OggDudeDataImporter.loadAction,
+            toggleDomainAction: OggDudeDataImporter.toggleDomainAction
+        },
+        footer: {
+            template: "templates/generic/form-footer.hbs",
+        },
+    }
+
+    /* -------------------------------------------- */
+
+    _prepareContext(options) {
+        //const setting = game.settings.get("swerpgSettings", "config");
+        console.log(`Preparing context: ${options}`, this);
+        return {
+            domains: this.domains,
+            domainSelectionDisabled: this.noZipFileSelected(),
+            loadButtonDisabled: this.noZipFileSelected() || this._noDomainSelected(),
+            zipFile: this.zipFile,
+        }
+    }
+
+    noZipFileSelected() {
+        return this.zipFile == null;
+    }
+
+    /* -------------------------------------------- */
+
+    _noDomainSelected() {
+        return this.domains.filter(domain => domain.checked).length === 0;
+    }
+
+    /* -------------------------------------------- */
+
+    _onRender(context, options) {
+        if (this.element.querySelector("#oggdude-zip-file") != null) {
+            this.element.querySelector("#oggdude-zip-file").addEventListener("change", this._onOggdudeZipFileChange.bind(this));
+        }
+        // We will deal with reset later
+    }
+
+    /* -------------------------------------------- */
+
+    static async loadAction(_event, target) {
+        console.log(`Load OggDude Data: {}`, this);
+        await OggDudeImporter.processOggDudeData(this.zipFile, this.domains);
+    }
+
+
+    /* -------------------------------------------- */
+    /**
+     * Convert a string to a boolean value.
+     * @param value {string} The value to convert.
+     * @returns {boolean|boolean} The converted value.
+     */
+    static toBoolean(value) {
+        return this ? value.toLowerCase() === "true" : false;
+    }
+
+    /* -------------------------------------------- */
+
+    static async toggleDomainAction(_event, target) {
+        if ( this.noZipFileSelected()){
+            return;
+        }
+        const name = target.dataset.domainName;
+        const value = OggDudeDataImporter.toBoolean(target.dataset.domainChecked);
+        console.log(`Toggle Domain [${name}/${value}]: {}`, _event, target);
+        this.domains = this.domains.map(domain => {
+            if (domain.id === name) {
+                domain.checked = !value;
+            }
+            return domain;
+        });
+        await this.render();
+    }
+
+    /* -------------------------------------------- */
+
+    async _onSubmit(event, form, formData) {
+        const settings = foundry.utils.expandObject(formData.object);
+        /*await Promise.all(
+            Object.entries(settings).map(([key, value]) => game.settings.set("foo", key, value))
+        );*/
+        console.log(`Saving settings: ${settings}`, this);
+    }
+
+    /* -------------------------------------------- */
+
+    static async resetAction(_event, target) {
+        //await game.settings.set("foo", "config", {});
+        console.log(`Resetting settings: {}`, this);
+        this.zipFile = null;
+        this.domains = this._initializeDomains(this._domainNames);
+        await this.render();
+    }
+
+    /* -------------------------------------------- */
+
+    /** @inheritdoc */
+    async close(options = {}) {
+        await super.close(options);
+    }
+
+    /* -------------------------------------------- */
+
+    /** @inheritdoc */
+    async _onOggdudeZipFileChange(event) {
+        this.zipFile = event.target.files[0];
+        console.log(`File changed: {}`, event, this.zipFile);
+        await this.render();
+    }
+
+    /* -------------------------------------------- */
+
+    /**
+     *
+     * @param {Event} event  The originating click event
+     * @private
+     */
+    async _onloadButtonClick(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const form = $("form.oggDude-data-importer")[0];
+        const importedFile = form['zip-file'].files[0];
+
+        await OggDudeImporter.processOggDudeData(importedFile, this.domains);
+
+        await this.close({});
+    }
+}
